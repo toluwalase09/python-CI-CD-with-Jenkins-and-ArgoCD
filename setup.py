8787b67bfa682aba14bc01fd3555f92dc62fe8f9
Toluwalase Adedotun <54019793+toluwalase09@users.noreply.github.com>
2023-02-15 01:31:32 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='myapp',
+    version='1.0.0',
+    author='Toluwalase Adedotun',
+    author_email='toluadedotun@gmail.com',
+    description='A brief description of your application',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cloudboostauk/python-CI-CD-with-Jenkins-and-ArgoCD',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'myapp=main:main'
+        ]
+    },
+    install_requires=install_requires
+)
